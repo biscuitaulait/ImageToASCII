929c8d1479458c7346726a8b52954bebac58f1ad
Biscuit Au Lait <102294019+biscuitaulait@users.noreply.github.com>
2022-06-24 18:47:38 +0300

    First commit

---
+++ ImageToASCII/ImageToASCIIConverter.cs
@@ -0,0 +1,33 @@
+﻿using System.Drawing;
+
+namespace ImageToASCII
+{
+    public class ImageToASCIIConverter
+    {
+        private readonly char[] table = new char[] {'.', ',', ':', '+', '*', '?', '%', 'S', '#', '@'};
+        private Bitmap bitmap;
+
+        public ImageToASCIIConverter(Bitmap bitmap)
+        {
+            this.bitmap = bitmap;
+        }
+
+        public char[][] Convert()
+        {
+            char[][] result = new char[bitmap.Height][];
+
+            for (int y = 0; y < bitmap.Height; y++)
+            {
+                result[y] = new char[bitmap.Width];
+                for (int x = 0; x < bitmap.Width; x++)
+                {
+                    result[y][x] = table[(int)Map(bitmap.GetPixel(x, y).R, 0, 255, 0, table.Length - 1)];
+                }
+            }
+
+            return result;
+        }
+
+        public float Map(float valueToMap, float start1, float stop1, float start2, float stop2) => (valueToMap - start1) / (stop1 - start1) * (stop2 - start2) + start2;
+    }
+}
---
+++ ImageToASCII/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Drawing;
+
+namespace ImageToASCII
+{
+    class Program
+    {
+        private const double WIDTH_OFFSET = 3;
+        private const int MAX_WIDTH = 500;
+        private static void Main()
+        {
+            Console.Write("Enter image path: ");
+            string path = Console.ReadLine();
+
+            Bitmap bitmap = new Bitmap(path);
+
+            bitmap = resizeBitmap(bitmap);
+            bitmap.ToGrayscale();
+
+            Console.Clear();
+            ImageToASCIIConverter converter = new ImageToASCIIConverter(bitmap);
+            char[][] rows = converter.Convert();
+
+            foreach (var row in rows)
+            {
+                Console.WriteLine(row);
+            }
+
+            Console.SetCursorPosition(0, 0);
+            Console.ReadLine();
+        }
+
+        public static Bitmap resizeBitmap(Bitmap bitmap)
+        {
+            double newHeight = bitmap.Height / WIDTH_OFFSET * MAX_WIDTH / bitmap.Width;
+            if (bitmap.Width > MAX_WIDTH || bitmap.Height > newHeight)
+                bitmap = new Bitmap(bitmap, new Size(MAX_WIDTH, (int)newHeight));
+            return bitmap;
+        }
+    }
+}
